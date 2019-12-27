c6cb45e73bce1956c1ccd369ac802f8fc2efaefd
rendyrey <rendyreynaldy@gmail.com>
2019-12-27 09:29:55 +0700

    ikop

--- resources/views/pelaksana/create.blade.php
+++ resources/views/pelaksana/create.blade.php
@@ -22,8 +22,8 @@
       <div class="breadcrumb-line breadcrumb-line-light header-elements-md-inline">
         <div class="d-flex">
           <div class="breadcrumb">
-            <a href="{{url('dashboard')}}" class="breadcrumb-item"><i class="icon-home2 mr-2"></i> Dashboard</a>
-            <a href="{{url('pelaksana')}}" class="breadcrumb-item">Pelaksana</a>
+            <a href="{{url($user->role.'/dashboard')}}" class="breadcrumb-item"><i class="icon-home2 mr-2"></i> Dashboard</a>
+            <a href="{{url($user->role.'/pelaksana')}}" class="breadcrumb-item">Pelaksana</a>
           </div>
           <a href="#" class="header-elements-toggle text-default d-md-none"><i class="icon-more"></i></a>
         </div>
