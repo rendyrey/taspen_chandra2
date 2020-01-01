28c63b2d21e2de906c83684bfefdf40ff62bb4b2
Nindya Sofiana <nindyasofi@gmail.com>
2020-01-01 14:03:05 +0700

    changed

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,6 +6,9 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<title>SIMKERJA - TASPEN</title>
 
+	<!-- favicon -->
+	<link rel="icon" href="{{asset('assets/images/taspen4.png')}}" type="image/x-icon"/>
+
 	<!-- Global stylesheets -->
 	<link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
 	<link href="{{asset('limitless/global_assets/css/icons/icomoon/styles.min.css')}}" rel="stylesheet" type="text/css">
