c046bc103b5329858226c2df00530dc369aee5ae
Rendy Reynaldy <rendyreynaldy@gmail.com>
2020-02-27 11:10:05 +0700

    test

--- resources/views/kepala/report.blade.php
+++ resources/views/kepala/report.blade.php
@@ -72,7 +72,7 @@
           </div>
           @if($user->role == 'kepala-satuan-kerja' || $user->role == 'wakil-kepala-satuan-kerja')
             <div class="form-group row">
-              <label class="col-lg-3 col-form-label">Bidang XXX</label>
+              <label class="col-lg-3 col-form-label">Bidang</label>
               <div class="col-lg-9">
                 {{Form::select('bidang_id',$bidang,null,['class'=>'form-control select-search data-fouc','data-placeholder'=>'Pilih Bidang'])}}
               </div>
